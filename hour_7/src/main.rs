7a0268d11a68127a74d9e8eeffb2931551ff9b66
enginearn <enginyaaaaan@gmail.com>
2022-06-28 04:25:29 +0900

    done

--- hour_7/src/main.rs
+++ hour_7/src/main.rs
@@ -1,3 +1,83 @@
 fn main() {
-    println!("Hello, world!");
+    // A vector is actually a dynamic or mutable array.
+    // It is a single data structure that can store multiple values in memory.
+    // There are three ways to create a vector.
+
+    // The first method to create a vector is:
+    // let vector_name = vec![value1, value2, value3];
+    let v = vec![100, 200, 300];
+    println!("{}", v[0]);
+    println!("{}", v[1]);
+    println!("{}", v[2]);
+
+    // The second method to create a vector is:
+    // let v = vec![value; repeat];
+    let v = vec![100; 3];
+    println!("First element is '{}'", v[0]);
+    println!("Second element is '{}'", v[1]);
+    println!("Third element is '{}'", v[2]);
+
+    // The third method to create a vector is:
+    // let mut v = Vec::new(); // use "new" keyword to create a vector.
+    // v.push(value1); // set value to vector.
+    let mut v = Vec::new(); // create a vector.
+    v.push('R'); // set value to vector.
+    v.push('u'); // set value to vector.
+    v.push('s'); // set value to vector.
+    v.push('t'); // set value to vector.
+    for c in v {
+        print!("{}", c);
+    }
+
+    // You can use "|" symbol to match multiple patterns.
+    let num: i32 = 5;
+    match num {
+        1 | 2 | 3 => println!("One, two, or three"),
+        _ => println!("Something else"),
+    }
+
+    // The symbol "..." can match the values within the specific range.
+    let x = 3;
+    match x {
+        1..=5 => println!("x is 1, 2, 3, 4, or 5"),
+        _ => println!("x is something else"),
+    }
+
+    // Binding a range
+    let x = 4;
+    match x {
+        1 => println!("one"),
+        var@2..=6 => println!("var is {}", var),
+        _ => println!("others"),
+    }
+
+    // Generics
+    // The argument in a function can accept multiple types of data.
+    // This can be done with generics. Generics are also known as parametric polymorphism.
+    // THE Rust standard library provides Option for generics.
+    // The syntax of the generics data type is:
+    // enum Optional<T> {
+    //     Some(T),
+    //     None,
+    // }
+    let x: Option<bool> = Some(true);
+    let y: Option<i32> = Some(10);
+    let z: Option<f64> = Some(3.14);
+    let n: Option<i32> = None;
+    match x {
+        Some(x) => println!("x is '{}'", x),
+        None => println!("x is 'None'"),
+    }
+    match y {
+        Some(y) => println!("y is '{}'", y),
+        None => println!("y is 'None'"),
+    }
+    match z {
+        Some(z) => println!("z is '{}'", z),
+        None => println!("z is 'None'"),
+    }
+    match n {
+        Some(n) => println!("n is '{}'", n),
+        None => println!("n is 'None'"),
+    }
 }
--- hour_8/src/main.rs
+++ hour_8/src/main.rs
@@ -1,3 +1,124 @@
+// A function in struct, enum, trait is called method.
+// The syntax to define a method is:
+// impl Struct/Enum {
+//     fn method_name(&self) -> type_of_return_value {
+//         // code
+//         self.member_namer // access the member variable.
+//     }
+// }
+/*
+The first letter of the struct name should be capitalized.
+The parameter "&self" indicates that the method is using a referenced parameter.
+Usually, if we use a referenced parameter in a method, then the method doesn't need to
+use "return" statement. The referenced parameter can return the value to the method caller.
+*/
+
+struct Circle {
+    radius: f64,
+}
+
+impl Circle {
+    fn area(&self) -> f64 {
+        std::f64::consts::PI * (self.radius * self.radius)
+    }
+}
+
+/*
+Trait is an interface in Rust, it defines a trait method without
+the body and is implemented by an "impl" method.
+*/
+// Define a trait and its method.
+// trait Trait_Name {
+//     fn trait_method(&self);
+// }
+// The first letter of the trait name should be capitalized.
+// The trait method has no body.
+
+// Implement the trait method.
+// impl Trait_Name for Struct/Enum {
+//     fn trait_method(&self) {
+//         // code
+//         self.member_namer // access the member variable.
+//     }
+// }
+
+trait TraitCalculateArea {
+    fn trait_area(&self) -> f64;
+}
+
+impl TraitCalculateArea for Circle {
+    fn trait_area(&self) -> f64 {
+        std::f64::consts::PI * (self.radius * self.radius)
+    }
+}
+
+pub trait Show {
+    fn show(&self);
+}
+
+impl<T> Show for T
+where
+    T: ToString,
+{
+    fn show(&self) {
+        println!("{}", self.to_string());
+    }
+}
+
+/*
+The drop() method will be automatically invoked when the last "_variable"
+expression finishes running.
+The feature of drop() method: "Last in, First out".
+*/
+// The syntax to define a drop() method is:
+// fn drop(&mut self) {
+//     // execute commands according to "Lat in , First out"
+// }
+
+// The syntax to define a "_variable" expression is:
+// let _variable = Strict {member: value}
+
+struct Game {
+    number: i32,
+}
+
+impl Drop for Game {
+    fn drop(&mut self) {
+        println!("The #{} Winner.", self.number);
+    } // drop method runs the code based on "Last in First out".
+}
+
+// The clousure is the anonymous function of Rust Language.
+// Create a closure or an anonymous function.
+// let closure_name = |parameter| {};
+
+// call the closure or anonymous function.
+// closure_name(parameter);
+
 fn main() {
-    println!("Hello, world!");
+    let circle = Circle { radius: 200.0 };
+    println!("The area of the circle is {}", circle.area());
+
+    let trait_area = Circle { radius: 150.00 };
+    println!("The Circle area is '{}'", trait_area.trait_area());
+
+    String::from("Rust in 80Hours").show();
+
+    let _baseball = Game { number: 3 }; // define _baseball
+                                        // _baseball expression runs last
+    let _football = Game { number: 2 };
+    // _football expression runs next
+    let _basketball = Game { number: 1 };
+    // _basketball expression runs first
+
+    let my_closure = |num: i32| num + 200;
+    let num = 100;
+    println!("{}", my_closure(num));
+
+    let mut capacity = "Hard disk capacity: 5000".to_string();
+    {
+        let mut my_closure = |c: char| capacity.push(c);
+        my_closure('G');
+    }
+    println!("{}", capacity);
 }
